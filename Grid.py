cafa0b97c94d201f80d963e80a2d277a4dcf18c2
Bhushan <bhushan1810patil@gmail.com>
2020-12-30 17:58:55 +0530

    add new methods to Grid
    draw lines, spot and get spot

--- Grid.py
+++ Grid.py
@@ -3,6 +3,7 @@ import os
 import settings
 import shelve
 from Spot import Spot
+from color import YELLOW
 from settings import GRID_COLS, GRID_ROWS, GRID_SIZE, GRID_OFFSET
 
 class Grid(object):
@@ -31,4 +32,18 @@ class Grid(object):
                 spot = Spot(row, col)
                 self.grid[row].append(spot)
 
-        
+    def draw_lines(self, win: pygame.surface) -> None:
+        for x in range(GRID_OFFSET, GRID_OFFSET + GRID_COLS * GRID_SIZE + 5, GRID_SIZE):
+            pygame.draw.line(win, YELLOW, (x, GRID_OFFSET), (x, 630), 1)
+
+        for y in range(GRID_OFFSET, GRID_OFFSET + GRID_ROWS * GRID_SIZE + 5, GRID_SIZE):
+            pygame.draw.line(win, YELLOW, (GRID_OFFSET, y), (650, y), 1)
+
+
+    def draw_grid(self, win: pygame.surface) -> None:
+        for row in self.grid:
+            for spot in row:
+                spot.draw(win)
+
+    def get_spot(self, row: int, col: int) -> Spot:
+        return self.grid[row][col]
