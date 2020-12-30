5ee6a3e7a34ae77171f2cba9d918f65530d680ec
Bhushan <bhushan1810patil@gmail.com>
2020-12-30 23:21:42 +0530

    set_wall something

--- set_wall.py
+++ set_wall.py
@@ -47,17 +47,19 @@ class Temp(object):
 
         win.blit(self.BG, (self.offset, self.offset))
         win.blit(movemap, (self.offset, self.offset))
+
+        
+
+        self.grid.draw_lines(win)
         pygame.display.update()
 
 
-    def isValidPos(self, row, col):
-        pass
 
     
     def run(self) -> None:
         
         self.grid_init()
-
+        wall = list()
         run = True
         while run:
             self.clock.tick(FPS)
@@ -66,6 +68,25 @@ class Temp(object):
 
                 if event.type == pygame.QUIT:
                     run = False
+
+                pressed = pygame.mouse.get_pressed()
+                
+                if pressed[0]:
+                    pos = pygame.mouse.get_pos()
+                    row, col = self.grid.get_grid_dims(pos)
+                    # print(row, col, sep='\t')
+
+                    if self.grid.is_valid_pos
+
+                if pressed[2]:
+                    pos = pygame.mouse.get_pos()
+                    row, col = self.grid.get_grid_dims(pos)
+
+                    if self.grid.is_valid_pos(row, col):
+                        pass
+
+
+                    
 
             self.draw(self.win)
 
