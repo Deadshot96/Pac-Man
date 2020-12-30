8103d6930cb86a1235b5ac28792d9ecb3fc0a4af
Bhushan <bhushan1810patil@gmail.com>
2020-12-30 15:28:51 +0530

    add methods in Spot
    need to add update_neighbours

--- Spot.py
+++ Spot.py
@@ -37,3 +37,51 @@ class Spot:
 
     def is_dot(self):
         return self.isDot
+
+    def make_wall(self):
+        self.isWall = True
+        self.color = BLACK
+
+    def is_wall(self):
+        return self.isWall
+
+    def make_path(self):
+        self.isWall = False
+        self.color = WHITE
+
+    def make_player(self):
+        self.isItem = True
+        self.color = YELLOW
+
+    def is_closed(self):
+        return self.isClosed
+
+    def close(self):
+        self.isClosed = True
+
+    def make_blinky(self):
+        self.isItem = True
+        self.color = pygame.color.THECOLORS['red2']
+
+    def make_pinky(self):
+        self.isItem = True
+        self.color = pygame.color.THECOLORS['pink2']
+
+    def make_inky(self):
+        self.isItem = True
+        self.color = pygame.color.THECOLORS['cyan2']
+
+    def make_clyde(self):
+        self.isItem = True
+        self.color = pygame.color.THECOLORS['yellow2']
+
+    def remove_item(self):
+        self.isItem = False
+        self.color = WHITE
+
+    def is_empty(self):
+        return not (self.isItem or self.isWall)
+
+    def update_neighbors(self):
+        pass
+    
