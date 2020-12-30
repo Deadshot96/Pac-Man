d81b65b183a397f8449b2a230b8639d6c0fa90d4
Bhushan <bhushan1810patil@gmail.com>
2020-12-31 01:25:02 +0530

    create and add Dot class

---
+++ Dot.py
@@ -0,0 +1,22 @@
+from settings import DOT_SIZE, DOT_OFFSET, DOT_LEN
+
+class Dot(object):
+
+    def __init__(self, row: int, col: int, img: pygame.Surface):
+        self.row = row
+        self.col = col
+        self.dot = img
+        self.x = self.col * DOT_SIZE + DOT_OFFSET
+        self.y = self.row * DOT_SIZE + DOT_OFFSET
+        self.isEaten = False
+
+    def draw(self, win: pygame.Surface) -> None:
+        win.blit(self.dot, (self.x, self.y))
+
+    def eat(self) -> None:
+        self.isEaten = True
+
+    def is_eaten(self) -> bool:
+        return self.isEaten
+
+    
