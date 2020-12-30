be973e5f34d8b3e963ce71f94493e257809d1e30
Bhushan <bhushan1810patil@gmail.com>
2020-12-31 01:15:09 +0530

    add Game class
    did a few methods

--- main.py
+++ main.py
@@ -7,3 +7,87 @@ import math
 import copy
 from queue import PriorityQueue
 import settings
+from color import BLACK
+from Grid import Grid
+class Game(object):
+
+    asset_dir = os.path.join(os.getcwd(), 'assets')
+    BG = pygame.image.load(os.path.join(asset_dir, 'colormap.png'))
+    MOVEMAP = pygame.image.load(os.path.join(asset_dir, 'movemap.png'))
+    DOTMAP = pygame.image.load(os.path.join(asset_dir, 'dotmap.png'))
+    DOT = pygame.image.load(os.path.join(asset_dir, 'dot.png'))
+    
+    def __init__(self):
+
+        self.rows = settings.GRID_ROWS
+        self.cols = settings.GRID_COLS
+        self.width = settings.WIDTH
+        self.height = settings.HEIGHT
+        self.size = settings.GRID_SIZE
+        self.offset = settings.GRID_OFFSET
+        self.fps = settings.FPS
+        self.grid = None
+        self.player = None
+        self.clock = None
+        self.win = None
+        self.showWall = False
+        
+        
+    def game_init(self) -> None:
+        pygame.init()
+        pygame.font.init()
+
+        self.win = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Pac-Man Developer")
+
+        self.grid = Grid()
+
+        self.clock = pygame.time.Clock()
+        
+        self.win.fill(BLACK)
+        pygame.display.update()
+
+
+    def draw(self, win: pygame.Surface) -> None:
+        win.blit(self.BG, (self.offset, self.offset))
+
+        if self.showWall:
+            self.grid.draw_path(self.win)
+
+        pygame.display.update()
+
+    def run(self) -> None:
+
+        self.game_init()
+        
+        run = True
+        while run:
+            self.clock.tick(self.fps)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    row, col = self.grid.get_grid_dims(pos)
+                    print(row, col, sep='\t')
+
+                if event.type == pygame.KEYDOWN:
+                    keys = pygame.key.get_pressed()
+
+                    if keys[pygame.K_SPACE]:
+                        self.showWall = not self.showWall
+
+            self.draw(self.win)
+
+        pygame.font.quit()
+        pygame.quit()
+
+
+
+if __name__ == '__main__':
+    X = Game()
+    X.run()
+
+
