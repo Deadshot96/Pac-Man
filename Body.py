a8a4d0d7efc6dbf9689251d422a17896f960359d
Bhushan <bhushan1810patil@gmail.com>
2021-01-01 12:49:30 +0530

    change body
    add a few methods .. add load images method

--- Body.py
+++ Body.py
@@ -1,14 +1,20 @@
 from typing import Tuple
 from pygame import Surface
+from settings import GRID_SIZE, GRID_OFFSET
+import pygame
+import os
 
 class Body(object):
 
     COOLDOWN = 5
     IMAGECOUNT = 0
-
+    asset_dir = os.path.join(os.getcwd(), 'assets')
+ 
     def __init__(self, name: str):
 
         self.name = name
+        self.row = 0
+        self.col = 0
         self.x = 0
         self.y = 0
         self.moveX = 0
@@ -19,6 +25,7 @@ class Body(object):
         self.animation_count = 0
         self.cooldown_count = 0
         self.imgDims = (32, 32)
+        self.image = None
 
         self.up = []
         self.down = []
@@ -35,11 +42,16 @@ class Body(object):
             self.cooldown_count += 1
 
     def get_pos(self) -> Tuple:
-        return self.x, self.y
+        return self.row, self.col
 
     def get_center_pos(self) -> Tuple:
         offset = self.imgDims[0] // 2
         return self.x + offset, self.y + offset
+
+    def set_x_and_y(self) -> None:
+        self.x = self.row * GRID_SIZE + GRID_OFFSET
+        self.y = self.col * GRID_SIZE + GRID_OFFSET
+
 
     def draw(self, win: Surface) -> None:
         if self.cooldown_count == 0:
@@ -52,3 +64,18 @@ class Body(object):
         self.image = self.angleDict[self.angle][self.animation_count]
         win.blit(self.image, (self.x, self.y))
         self.cooldown()
+
+    def load_images(self) -> None:
+        img_folder_path = os.path.join(self.asset_dir, self.name)
+
+        for filename in os.listdir(img_folder_path):
+            pic = pygame.transform.scale(pygame.image.load(os.path.join(img_folder_path, filename)), self.imgDims)
+            
+            if filename.startswith('up'):
+                self.up.append(pic)
+            elif filename.startswith('down'):
+                self.down.append(pic)
+            elif filename.startswith('right'):
+                self.right.append(pic)
+            elif filename.startswith('left'):
+                self.left.append(pic)
