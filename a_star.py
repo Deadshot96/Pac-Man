9b275e98a7027fc5950e6a359ffc5724e70a46e8
Bhushan <bhushan1810patil@gmail.com>
2021-01-02 18:13:48 +0530

    created a_star

---
+++ a_star.py
@@ -0,0 +1,9 @@
+import pygame
+from settings import GRID_ROWS, GRID_COLS
+from queue import PriorityQueue
+from Grid import Grid
+from Body import Body
+from Player import Player
+
+def a_star(self, grid: Grid, ghost: Body, player: Player):
+    pass
